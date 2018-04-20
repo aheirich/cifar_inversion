bf51f166de915a8427fa021d074b401a6581815d
Alan Heirich <heirich@alumni.caltech.edu>
2018-04-20 13:00:10 -0700

    use keras dataset for cifar-10 copy code from http://parneetk.github.io/blog/cnn-cifar10/

--- train.py
+++ train.py
@@ -5,8 +5,10 @@ import keras
 from keras.layers import Dense, Conv2D, Reshape, Flatten
 from keras.models import Sequential
 from keras.optimizers import Adadelta
+from keras.utils import np_utils
 import matplotlib.pylab as plt
 import numpy
+import time
 
 inputWidth = 32 * 32 * 3
 layerWidth = [ 96, 96, 192, 192, 192, 192 ]
@@ -17,90 +19,97 @@ denseLayerWidth = 256
 numClasses = 10 # cifar-10
 convolutionSize = 3
 batchSize = 128
-epochs = 50
+epochs = 1
 numBatches = 5
 
 
-def unpickle(file):
-  import cPickle
-  with open(file, 'rb') as fo:
-    dict = cPickle.load(fo)
-  return dict
+
+
 
 def createModel():
   model = Sequential()
   for i in range(len(layerWidth)):
     width = layerWidth[i]
     if i == 0:
-      model.add(Conv2D(inputWidth, convolutionSize, activation='relu',
+      model.add(Conv2D(inputWidth, convolutionSize,
+                       activation='relu',
                        input_shape=inputShape))
     else:
-      model.add(Conv2D(inputWidth, convolutionSize, activation='relu'))
+      model.add(Conv2D(inputWidth, convolutionSize,
+                       activation='relu'))
   model.add(Flatten())
   model.add(Dense(denseLayerWidth, activation='relu'))
   model.add(Dense(numClasses, activation='relu'))
-  model.compile(loss=keras.losses.mse,
-                optimizer=Adadelta(),
+  model.compile(loss='categorical_crossentropy',
+                optimizer='Adadelta',
                 metrics=['accuracy'])
   return model
 
-class AccuracyHistory(keras.callbacks.Callback):
-  def on_train_begin(self, logs={}):
-    self.acc = []
-  def on_epoch_end(self, batch, logs={}):
-    self.acc.append(logs.get('acc'))
-
-def oneHot(labels):
-  result = []
-  for label in labels:
-    y = [0] * numClasses
-    y[label] = 1
-    result.append(y)
-  return numpy.array(result)
-
-def loadData():
-  result = []
-  for batch in range(numBatches):
-    filename = 'cifar-10-batches-py/data_batch_' + str(batch + 1)
-    print 'get batch', filename
-    dict = unpickle(filename)
-    x_train = dict['data'].reshape(batchShape)
-    y_train = oneHot(dict['labels'])
-    subbatchSize = 1000
-    numSubbatches = len(y_train) / subbatchSize
-    for i in range(numSubbatches):
-      x_subbatch = x_train[i * subbatchSize : (i + 1) * subbatchSize]
-      y_subbatch = y_train[i * subbatchSize : (i + 1) * subbatchSize]
-      result.append((x_subbatch, y_subbatch))
-  return result
 
 
 
-training_data = loadData()
 
-test_data = unpickle('cifar-10-batches-py/test_batch')
-x_test = test_data['data'].reshape(batchShape)
-y_test = oneHot(test_data['labels'])
+
+def plot_model_history(model_history):
+  fig, axs = plt.subplots(1,2,figsize=(15,5))
+  # summarize history for accuracy
+  axs[0].plot(range(1,len(model_history.history['acc'])+1),model_history.history['acc'])
+  axs[0].plot(range(1,len(model_history.history['val_acc'])+1),model_history.history['val_acc'])
+  axs[0].set_title('Model Accuracy')
+  axs[0].set_ylabel('Accuracy')
+  axs[0].set_xlabel('Epoch')
+  axs[0].set_xticks(numpy.arange(1,len(model_history.history['acc'])+1),len(model_history.history['acc'])/10)
+  axs[0].legend(['train', 'val'], loc='best')
+  # summarize history for loss
+  axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+  axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+  axs[1].set_title('Model Loss')
+  axs[1].set_ylabel('Loss')
+  axs[1].set_xlabel('Epoch')
+  axs[1].set_xticks(numpy.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+  axs[1].legend(['train', 'val'], loc='best')
+  plt.show()
+
+
+
+def accuracy(test_x, test_y, model):
+  result = model.predict(test_x)
+  predicted_class = numpy.argmax(result, axis=1)
+  true_class = numpy.argmax(test_y, axis=1)
+  num_correct = numpy.sum(predicted_class == true_class)
+  accuracy = float(num_correct)/result.shape[0]
+  return (accuracy * 100)
+
+
+
+
+
+from keras.datasets import cifar10
+(train_features, train_labels), (test_features, test_labels) = cifar10.load_data()
+num_train, img_channels, img_rows, img_cols =  train_features.shape
+num_test, _, _, _ =  test_features.shape
+num_classes = len(numpy.unique(train_labels))
+
+
+train_features = train_features.astype('float32')/255
+test_features = test_features.astype('float32')/255
+# convert class labels to binary class labels
+train_labels = np_utils.to_categorical(train_labels, num_classes)
+test_labels = np_utils.to_categorical(test_labels, num_classes)
+
 
 model = createModel()
 
-history = AccuracyHistory()
 
-for batch in training_data:
-  print 'training a batch'
-  (x_train, y_train) = batch
-  model.fit(x_train, y_train,
-            batch_size=batchSize,
-            epochs=epochs,
-            verbose=1,
-            validation_data=(x_test, y_test),
-            callbacks=[history])
+start = time.time()
+print 'starting time', start
+model_info = model.fit(train_features, train_labels,
+        batch_size=batchSize,
+        epochs=epochs,
+        verbose=1,
+        validation_data=(test_features, test_labels))
+end = time.time()
+print 'ending time', end
+plot_model_history(model_info)
 
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-plt.plot(range(1, 11), history.acc)
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.show()
 
